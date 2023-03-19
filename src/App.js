cd6c93844e8e69eac31daac50fabd34961b91087
Isela Alarcón <isela.v.alarcon@gmail.com>
2023-03-19 16:58:52 +0100

    dividing the app into components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import "./style.css";
 
 function App() {
+  const AppTitle = "Today I Learned";
   return (
-    <header className="header">
-      <div className="logo">
-        <img
-          src="logo.png"
-          height="68px"
-          width="68px"
-          alt="Today I learned logo"
-        />
-        <h1>Today I learned</h1>
-      </div>
-      <button className="btn btn-large btn-open">Share a fact</button>
-    </header>
+    <>
+      {/*HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img
+            src="logo.png"
+            height="68px"
+            width="68px"
+            alt="Today I learned logo"
+          />
+          <h1>{AppTitle}</h1>
+        </div>
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+      <NewFactForm />
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
   );
+}
+function NewFactForm() {
+  return <form className="fact-form"> Fact form </form>;
+}
+function CategoryFilter() {
+  return <aside>Category Filter</aside>;
+}
+function FactList() {
+  return <section>Facts List</section>;
 }
 
 export default App;
