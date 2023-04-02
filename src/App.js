dbe4895120e45b624625df04cd0aa555e37780d0
Isela <isela.v.alarcon@gmail.com>
2023-04-01 20:07:43 -0600

    finish touches

--- src/App.js
+++ src/App.js
@@ -281,6 +281,7 @@ function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
   const isDisputed =
     fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
+
   async function handleVote(columnName) {
     setIsUpdating(true);
     const { data: updatedFact, error } = await supabase
@@ -298,7 +299,7 @@ function Fact({ fact, setFacts }) {
   return (
     <li className="fact">
       <p>
-        {isDisputed ? <span className="disputed">[DISPUTED]</span> : null}
+        {isDisputed ? <span className="disputed">[⛔️ DISPUTED]</span> : null}
         {fact.text}
 
         <a className="source" href={fact.source} target="_blank">
